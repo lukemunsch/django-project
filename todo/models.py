ee5b428bba7062824ed9c82e57a484ce1ebeeb05
lukemunsch <lpmunsch@gmail.com>
2022-01-16 21:19:14 +0000

    add context for new add item page

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
-# Create your models here.
 
+# Create your models here.
 class Item(models.Model):
+    """
+    Creates the model class for the item table
+    """
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
+        """Change the default value item object to item name specific"""
         return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,18 @@
+"""this page is to create renders of our web pages"""
 from django.shortcuts import render
+from .models import Item
+
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    """processing our todo list render for viewing a webpage"""
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    """processing our add item to render a view"""
+    return render(request, 'todo/add_item.html')
