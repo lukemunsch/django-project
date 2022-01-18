695477fb205e34d5c228dcef4812ad36634b28cc
lukemunsch <lpmunsch@gmail.com>
2022-01-18 13:23:04 +0000

    create new test files and fill with content

--- todo/test_models.py
+++ todo/test_models.py
@@ -4,4 +4,4 @@ from django.test import TestCase
 class TestDjango(TestCase):
 
     def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+        pass
---
+++ todo/test_views.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        
